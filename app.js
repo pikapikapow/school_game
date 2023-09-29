ad715954fafd576a703a6ac94038ff8d5701e2ab
pikapikapow <vladimirhwang84@gmail.com>
2023-09-29 19:07:23 -0400

    comparing cards system finished

--- app.js
+++ app.js
@@ -2,9 +2,58 @@ const cards = document.querySelectorAll(".card")
 
 console.log(cards)
 
+// card comparing logic
+
+let firstCard = null
+let secondCard = null
+let hasFlippedCard = false
+
+
+
+// card flip function
+
 function flipCard() {
-  this.classList.toggle("flip")
+  this.classList.add("flip")
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true
+    firstCard = this
+    return
+  } else {
+    secondCard = this
+    hasFlippedCard = false
+  }
+  checkCards()
+
+
 }
 
+function checkCards() {
+  console.log(firstCard, secondCard);
+  if (firstCard.dataset.planet === secondCard.dataset.planet) {
+    disableCards()
+    return
+  } else {
+    unflipCards()
+  }
+
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard)
+  secondCard.removeEventListener("click", flipCard)
+}
+
+function unflipCards() {
+  setTimeout(function () {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+  }, 1500);
+}
+
+
+
 cards.forEach(card => card.addEventListener("click", flipCard))
+
+
 
