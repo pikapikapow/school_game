c2aa8aa557688619a587f4e3fd547798d5698a4f
pikapikapow <vladimirhwang84@gmail.com>
2024-03-03 13:57:54 -0500

    Added 2 new levels

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const cards = document.querySelectorAll(".card")
-const restart = document.getElementById("restart-btn")
-const counterEl = document.getElementById("count")
+const cards = document.querySelectorAll(".card");
+const restart = document.getElementById("restart-btn");
+const counterEl = document.getElementById("count");
 // model elements
 const close = document.getElementById("close");
 const modal = document.getElementById("modal");
-
 
 // console.log(counterEl);
 
@@ -12,109 +11,92 @@ const modal = document.getElementById("modal");
 
 // card comparing logic
 
-let firstCard = null
-let secondCard = null
-let hasFlippedCard = false
-let blockBoard = false
-let counter = 0
-let matchedCards = 0
-
-
+let firstCard = null;
+let secondCard = null;
+let hasFlippedCard = false;
+let blockBoard = false;
+let counter = 0;
+let matchedCards = 0;
 
 // card flip function
 
 function flipCard() {
-
-  if (blockBoard) return
-  this.classList.add("flip")
+  if (blockBoard) return;
+  this.classList.add("flip");
 
   if (!hasFlippedCard) {
-    hasFlippedCard = true
-    firstCard = this
-    firstCard.removeEventListener("click", flipCard)
-    firstCard.style.cursor = "default"
-    return
+    hasFlippedCard = true;
+    firstCard = this;
+    firstCard.removeEventListener("click", flipCard);
+    firstCard.style.cursor = "default";
+    return;
   } else {
-    firstCard.addEventListener("click", flipCard)
-    firstCard.style.cursor = "pointer"
-    secondCard = this
-    hasFlippedCard = false
-    counter++
+    firstCard.addEventListener("click", flipCard);
+    firstCard.style.cursor = "pointer";
+    secondCard = this;
+    hasFlippedCard = false;
+    counter++;
 
-    counterEl.innerHTML = counter
+    counterEl.innerHTML = counter;
   }
 
-  checkCards()
+  checkCards();
 }
-
-
-
 
 function checkCards() {
   if (firstCard.dataset.planet === secondCard.dataset.planet) {
-    disableCards()
-    matchedCards = matchedCards + 2
+    disableCards();
+    matchedCards = matchedCards + 2;
     if (matchedCards === 16) {
-      winnerGreet()
+      winnerGreet();
     }
-    return
+    return;
   } else {
-    unflipCards()
+    unflipCards();
   }
 }
 
 function winnerGreet() {
   // console.log("all matched!");
   const modalShow = () => modal.classList.add("show-modal");
-  modalShow()
+  modalShow();
   setTimeout(function () {
-    cards.forEach((card) => card.classList.remove("flip"))
-    startOver()
-  }, 2000)
-
+    cards.forEach((card) => card.classList.remove("flip"));
+    startOver();
+  }, 2000);
 }
 
-
-
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard)
-  secondCard.removeEventListener("click", flipCard)
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 }
 
 function unflipCards() {
-  blockBoard = true
+  blockBoard = true;
   setTimeout(function () {
-    firstCard.classList.remove('flip')
-    secondCard.classList.remove('flip')
-    blockBoard = false
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    blockBoard = false;
   }, 1000);
 }
 
-
 (function randomize() {
   cards.forEach((card) => {
-    let randomizeNumber = Math.floor(Math.random() * cards.length)
-    card.style.order = randomizeNumber
-  })
-})()
-
+    let randomizeNumber = Math.floor(Math.random() * cards.length);
+    card.style.order = randomizeNumber;
+  });
+})();
 
 function startOver() {
-  window.location.reload()
+  window.location.reload();
 }
 
-
-
-cards.forEach((card) => card.addEventListener("click", flipCard))
+cards.forEach((card) => card.addEventListener("click", flipCard));
 
 if (restart) {
-  restart.addEventListener("click", startOver)
+  restart.addEventListener("click", startOver);
 }
-
 
 if (close) {
   close.addEventListener("click", () => modal.classList.remove("show-modal"));
 }
-
-
-
